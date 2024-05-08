586509f5b459043bbc10fc7c4d4cd7f292021341
justDaniel2002 <todentsukanai@gmail.com>
2024-05-09 00:14:18 +0700

    minor update

--- WebTruyenFE/src/components/navbar.jsx
+++ WebTruyenFE/src/components/navbar.jsx
@@ -5,7 +5,7 @@ import { DropdownNav } from "./dropdownNav";
 export const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex justify-around px-20 py-10 items-center  lg:max-w-screen-xl m-auto">
+    <div className="flex justify-around px-20 py-10 items-center bg-blue-950 text-white  m-auto">
       <Link to={"/"}>
           <img src={minilogo} className="w-12" />
         </Link>
@@ -15,9 +15,7 @@ export const Navbar = () => {
       />
       <DropdownNav content={"Thể loại"} list={["Tiên hiệp", "Kiếm hiệp"]} />
       <DropdownNav content={"Tủ sách"} list={["Truyện đã đọc", "Truyện đã thích"]} />
-      <div className="flex bg-white text-black">
-        <input className="px-3 py-1" placeholder="Search"/> 
-      </div>
+      
 
         <div className="flex">
           <div className="flex bg-white text-black mr-10">
@@ -39,6 +37,6 @@ export const Navbar = () => {
           </div>
         </div>
       </div>
-    </div>
+    
   );
 };
