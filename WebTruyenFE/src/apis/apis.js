4e0f45ef98fd344a56e1561fdfb4092d5e70a23d
justDaniel2002 <todentsukanai@gmail.com>
2024-05-11 00:44:01 +0700

    Update

---
+++ WebTruyenFE/src/apis/apis.js
@@ -0,0 +1,3 @@
+export const AuthRegister = "http://localhost:5065/api/Auth/Register"
+export const GetCategories = "http://localhost:5065/api/Home/getAllCategories"
+export const GetStories = "https://localhost:7226/api/Home/getAllStories"
